871882b4fc801150e7e04376cd168108590b2554
V Tsar <vastsareg@gmail.com>
2024-04-12 21:53:25 +0300

    start

---
+++ data/objects.py
@@ -0,0 +1,17 @@
+import sqlalchemy
+from sqlalchemy import orm
+
+from .db_session import SqlAlchemyBase
+
+
+class Objects(SqlAlchemyBase):
+    __tablename__ = 'objects'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    information = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    similar = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("users.id"))
+    user = orm.relationship('User')
