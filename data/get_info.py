50ce53f50d09bca644c14ccaf5a537658e9ccb4c
V Tsar <vastsareg@gmail.com>
2024-04-29 13:37:41 +0300

    почти финал

--- data/get_info.py
+++ data/get_info.py
@@ -2,11 +2,20 @@ import sqlite3
 import random
 
 
-ids = random.sample(range(1, 4), 2)
-id1, id2 = int(ids[0]), int(ids[1])
-con = sqlite3.connect('db/architecture.db')
-cur = con.cursor()
-inf1, img1 = cur.execute("""SELECT information, image FROM Objects
-        WHERE id = ?""", (id1,)).fetchall()[0]
-inf2, img2 = cur.execute("""SELECT information, image FROM Objects
-        WHERE id = ?""", (id2,)).fetchall()[0]
+def get_info():
+    con = sqlite3.connect('db/architecture.db')
+    cur = con.cursor()
+    max_id = cur.execute("""SELECT MAX(id) FROM Objects""").fetchone()[0]
+    ids = random.sample(range(1, max_id + 1), 2)
+    id1, id2 = int(ids[0]), int(ids[1])
+    inf1, img1 = cur.execute("""SELECT information, image FROM Objects
+            WHERE id = ?""", (id1,)).fetchall()[0]
+    inf2, img2 = cur.execute("""SELECT information, image FROM Objects
+            WHERE id = ?""", (id2,)).fetchall()[0]
+    return {'id1': id1,
+            'id2': id2,
+            'img1': img1,
+            'img2': img2,
+            'inf1': inf1,
+            'inf2': inf2
+            }
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 from data import db_session
 from forms.register_login import RegisterForm, LoginForm
 from data.objects import Objects
 from data.users import User
 from flask_login import LoginManager, login_user, login_required, logout_user
-from data.get_info import id1, id2, inf1, inf2, img1, img2
+from data.get_info import get_info
+from map_builder import load_map_image
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+number_of_images = 0
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -50,12 +52,14 @@ def logout():
 def index():
     db_sess = db_session.create_session()
     objects = db_sess.query(Objects)
-    kwargs = {'id1': id1,
-              'id2': id2,
-              'img1': url_for('static', filename=f'img/{img1}'),
-              'img2': url_for('static', filename=f'img/{img2}'),
-              'inf1': inf1,
-              'inf2': inf2,
+    if request.method == 'GET':
+        session['info'] = get_info()
+    kwargs = {'id1': session['info'].get('id1'),
+              'id2': session['info'].get('id2'),
+              'img1': url_for('static', filename=f"img/{session['info'].get('img1')}"),
+              'img2': url_for('static', filename=f"img/{session['info'].get('img2')}"),
+              'inf1': session['info'].get('inf1'),
+              'inf2': session['info'].get('inf2'),
               'choose_image': False,
               'similar': []
               }
@@ -64,8 +68,11 @@ def index():
     elif request.method == 'POST':
         if request.form:
             kwargs['choose_image'] = True
-            similar_objects = db_sess.query(Objects).get(request.form['group1']).similar.split(', ')
+            chosen_object = db_sess.query(Objects).get(request.form['group1'])
+            similar_objects = chosen_object.similar.split(', ')
+            address = chosen_object.address
             kwargs['similar'] = similar_objects
+            session['address'] = address
             return render_template("index2.html", objects=objects, **kwargs)
         else:
             return redirect(url_for(f'map/{request.form["group1"]}'))
@@ -93,6 +100,17 @@ def reqister():
         db_sess.commit()
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/show_map', methods=['GET'])
+def show_map():
+    global number_of_images
+    if 'address' in session:
+        load_map_image(session['address'], f'map_{number_of_images}.png')
+        session['image_name'] = f'map_{number_of_images}.png'
+        number_of_images += 1
+    return render_template('show_map.html', image_name=url_for('static',
+                                                               filename=f"img/{session.get('image_name')}"))
 
 
 @app.route('/load', methods=['GET', 'POST'])
