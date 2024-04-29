b4722dcbf68d58614551428aa2f92e781b19aa55
V Tsar <vastsareg@gmail.com>
2024-04-29 11:03:18 +0300

    оптимизация, картинки починились

--- data/get_info.py
+++ data/get_info.py
@@ -2,8 +2,8 @@ import sqlite3
 import random
 
 
-id1 = int(random.sample(range(1, 4), 1)[0])
-id2 = int(random.sample(range(1, 4), 1)[0])
+ids = random.sample(range(1, 4), 2)
+id1, id2 = int(ids[0]), int(ids[1])
 con = sqlite3.connect('db/architecture.db')
 cur = con.cursor()
 inf1 = cur.execute("""SELECT information FROM Objects
@@ -11,5 +11,4 @@ inf1 = cur.execute("""SELECT information FROM Objects
 inf2 = cur.execute("""SELECT information FROM Objects
         WHERE id = ?""", (id2,)).fetchone()[0]
 filenames = ['Narkomfin.jpg', 'Kolomenskoye.jpg', 'Ascension.jpg', 'Simonov.jpg']
-img1 = filenames[id1]
-img2 = filenames[id2]
+img1, img2 = filenames[id1 - 1], filenames[id2 - 1]
